9b829860552253a3997b1a777a34215ec6f7995e
JohnNils Olson <johnnils@gmail.com>
2020-09-11 11:30:33 -0700

    update to work with array of objects

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -4,19 +4,30 @@ import Drink from "./Drink";
 import Container from "react-bootstrap/Container";
 
 function DrinkList(props) {
+  const masterDrinkList = [
+    {
+      name: "Prismatic",
+      brewer: "Ninkasi",
+      type: "Juicy IPA",
+      alcoholContent: "5.9%"
+    },
+    {
+      name: "Tricerahops",
+      brewer: "Ninkasi",
+      type: "Double IPA",
+      alcoholContent: "8.0%"
+    }
+  ]
   return (
     <React.Fragment>
       <Container fluid>
-        <Drink
-          name="Prismatic"
-          brewer="Ninkasi"
-          type="Juicy IPA" 
-          alcoholContent= "5.9%"/>
-        <Drink
-          name= "Tricerahops"
-          brewer= "Ninkasi"
-          type= "Double IPA"
-          alcoholContent="8.0%"/>
+        {masterDrinkList.map((drink, index) =>
+          <Drink
+            name={drink.name}
+            brewer={drink.brewer}
+            type={drink.type}
+            alcoholContent={drink.alcoholContent} />
+        )}
       </Container>
     </React.Fragment>
   );
