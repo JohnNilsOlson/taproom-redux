4f50b64a61f02b5e1689f47b59a8e547a33a7108
JohnNils Olson <johnnils@gmail.com>
2020-09-11 09:36:35 -0700

    remove boilerplate code, add header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container"
 
 function App() {
+
+  const containerStyles = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container style={containerStyles}>
+        <Header />
+      </Container>
+    </React.Fragment>
   );
 }
 
