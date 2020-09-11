1724245c97978ca0f5f419b948de3a95906dc223
JohnNils Olson <johnnils@gmail.com>
2020-09-11 11:23:26 -0700

    add proptypes, update formatting and style

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Row from "react-bootstrap/Row"
 
 function Drink(props){
+
   const drink={
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    border: "solid",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "lightgray",
+    padding: 10,
+    marginTop: 10
   }
   return (
     <React.Fragment>
@@ -15,11 +23,16 @@ function Drink(props){
         <h3>{props.name} - {props.alcoholContent}</h3>
         <h5>{props.type}</h5>
         <h5>{props.brewer}</h5>
-        <p>{props.description}</p>
-        <p>{props.quantity}</p>
       </Row>
     </React.Fragment>
   );
+}
+
+Drink.propTypes = {
+  name: PropTypes.string,
+  alcoholContent: PropTypes.number,
+  type: PropTypes.string,
+  brewer: PropTypes.string
 }
 
 export default Drink;
