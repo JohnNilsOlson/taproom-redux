a74063ec21c3003c6a7b455121ad43c771dc6068
JohnNils Olson <johnnils@gmail.com>
2020-09-11 12:56:51 -0700

    add drink detail component

---
+++ src/components/DrinkDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function DrinkDetail(props) {
+  return (
+    <React.Fragment>
+      <h1>Tap Detail</h1>
+    </React.Fragment>
+  );
+}
+
+export default DrinkDetail;
