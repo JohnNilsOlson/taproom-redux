97739b58e81423b1cc1fa3b346f066dd2b5bf3ca
JohnNils Olson <johnnils@gmail.com>
2020-09-11 11:46:04 -0700

    add dynamic button to switch view between form and list

--- src/components/DrinkControl.js
+++ src/components/DrinkControl.js
@@ -1,19 +1,38 @@
 import React from "react";
 import DrinkList from "./DrinkList";
+import NewDrinkForm from "./NewDrinkForm";
 
-import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 class DrinkControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisible: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisible: !prevState.formVisible
+    }));
   }
 
   render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisible) {
+      currentlyVisibleState = <NewDrinkForm />
+      buttonText = "Return to Tap List";
+    } else {
+      currentlyVisibleState = <DrinkList />
+      buttonText = "Add new Tap";
+    }
     return (
       <React.Fragment>
-        <DrinkList />
+        <Button variant="outline-dark" onClick={this.handleClick} block>{buttonText}</Button>
+        {currentlyVisibleState}
       </React.Fragment>
     );
   }
