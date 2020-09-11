3e7a69e5fb07b286196643b3ddf9e7f9f7ac3e7f
JohnNils Olson <johnnils@gmail.com>
2020-09-11 11:36:35 -0700

    add placeholder drink form component

---
+++ src/components/NewDrinkForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewDrinkForm(props){
+  return (
+    <React.Fragment>
+      <h1>Placeholder</h1>
+    </React.Fragment>
+  );
+}
+
+export default NewDrinkForm;
