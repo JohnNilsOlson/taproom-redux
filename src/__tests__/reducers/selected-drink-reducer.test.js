f303cb881d0f01ef1375d63bdb7d36f6d2a3a786
JohnNils Olson <johnnils@gmail.com>
2020-09-18 13:05:12 -0700

    add failing test for select drink action

--- src/__tests__/reducers/selected-drink-reducer.test.js
+++ src/__tests__/reducers/selected-drink-reducer.test.js
@@ -2,7 +2,23 @@ import selectedDrinkReducer from '../../reducers/selected-drink-reducer';
 
 describe('selectedDrinkReducer', () => {
 
-  test("Should return default state if no action is recognized", () => {
+  let action;
+
+  const drinkData = {
+    name: 'Test Drink',
+    drinkType: 'Test Type',
+    brewer: 'Test Brewery',
+    alcoholContent: 5.0,
+    description: 'Test Description',
+    quantity: 124,
+    id: 1
+  }
+
+  test('Should return default state if no action is recognized', () => {
     expect(selectedDrinkReducer(null, { type: null })).toEqual(null);
+  });
+
+  test('Should return selected drink', () => {
+    expect(selectedDrinkReducer(null, { type: 'SELECT_DRINK' })).toEqual(drinkData);
   });
 });
