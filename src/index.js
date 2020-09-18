b8956070fa0eef22fe77ea17a7d22696137e35de
JohnNils Olson <johnnils@gmail.com>
2020-09-18 09:54:08 -0700

    fix provider import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers/drink-list-reducer';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
